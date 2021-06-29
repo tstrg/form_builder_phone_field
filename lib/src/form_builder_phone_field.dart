45f20846258896f69e61614ad12ddff1a0c1a53f
tstrg <silnyt@seznam.cz>
2021-06-29 09:04:05 +0200

    Update form_builder_phone_field.dart

--- lib/src/form_builder_phone_field.dart
+++ lib/src/form_builder_phone_field.dart
@@ -60,7 +60,7 @@ class FormBuilderPhoneField extends FormBuilderField<String> {
     FormFieldValidator<String>? validator,
     String? initialValue,
     InputDecoration decoration = const InputDecoration(),
-    ValueChanged<String>? onChanged,
+    ValueChanged<String?>? onChanged,
     ValueTransformer<String>? valueTransformer,
     bool enabled = true,
     FormFieldSetter<String>? onSaved,
